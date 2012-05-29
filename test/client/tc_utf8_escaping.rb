f79931f9e348e5cf0769a9ace235373d5b3a7e02
Chris Beer <chris@cbeer.info>
2012-05-29 13:33:36 -0700

    fix invalid utf-8 test under ruby 1.9

--- test/client/tc_utf8_escaping.rb
+++ test/client/tc_utf8_escaping.rb
@@ -5,6 +5,7 @@ class UTF8Test < Test::Unit::TestCase
   def test_escaping_invalid_utf_8_characters
     client = OAI::Client.new 'http://localhost:3333/oai' #, :parser => 'libxml'
     invalid_utf_8 = [2, 3, 4, 104, 5, 101, 6, 108, 66897, 108, 66535, 111, 1114112, 33, 55234123, 33].pack("U*")
+    invalid_utf_8 = invalid_utf_8.force_encoding("binary") if invalid_utf_8.respond_to? :force_encoding
     assert_equal("hello!!", client.send(:strip_invalid_utf_8_chars, invalid_utf_8).gsub(/\?/, ''))
   end
 

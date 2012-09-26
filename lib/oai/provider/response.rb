a86db36778381053f5e53a834297ecf528040453
Tim Dettrick <t.dettrick@uq.edu.au>
2012-09-26 12:50:33 +1000

    Making schemaLocation whitespace so simple that any parser can handle it.

--- lib/oai/provider/response.rb
+++ lib/oai/provider/response.rb
@@ -49,7 +49,7 @@ module OAI
         'xmlns' => "http://www.openarchives.org/OAI/2.0/",
         'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
         'xsi:schemaLocation' => %{http://www.openarchives.org/OAI/2.0/
-          http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd}
+          http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd}.gsub(/\s+/, ' ')
       }
     end
     def extract_identifier(id)

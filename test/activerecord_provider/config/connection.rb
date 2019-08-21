098bb566db50dc75a5aadbcacbda1e90badabe5a
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-08-21 13:04:35 -0400

    one more try to get tests passing on jruby

--- test/activerecord_provider/config/connection.rb
+++ test/activerecord_provider/config/connection.rb
@@ -3,6 +3,12 @@ require 'logger'
 
 # Configure AR connection
 #ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+if RUBY_PLATFORM == "java"
+  require 'jdbc/sqlite3'
+  Jdbc::SQLite3.load_driver
+end
+
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.establish_connection :adapter => "sqlite3",
                                         :database => ":memory:"

5aedbf7bd38b122e68bffa80dc3536e1ae6b107d
Benjamin Armintor <armintor@gmail.com>
2022-04-25 12:39:09 -0400

    remove use of instance_variable_set in tests - fixes #98

--- test/provider/tc_functional_tokens.rb
+++ test/provider/tc_functional_tokens.rb
@@ -5,11 +5,6 @@ class ResumptionTokenFunctionalTest < Test::Unit::TestCase
 
   def setup
     @provider = ComplexProvider.new
-    @provider.model.instance_variable_set(:@limit, 120)
-  end
-
-  def teardown
-    @provider.model.instance_variable_set(:@limit, 100)
   end
 
   def test_resumption_tokens
@@ -26,7 +21,8 @@ class ResumptionTokenFunctionalTest < Test::Unit::TestCase
   end
 
   def test_from_and_until_with_resumption_tokens
-    # Should return 300 records broken into 3 groups of 120, 120, and 60.
+    # Should return 550 records broken into 5 groups of 100, and a final group of 50.
+    # checked elements under ListRecords are limit + 1, accounting for the resumptionToken element
     assert_nothing_raised do
       Document.new(@provider.list_records(:metadata_prefix => 'oai_dc'))
     end
@@ -34,20 +30,22 @@ class ResumptionTokenFunctionalTest < Test::Unit::TestCase
       @provider.list_records(
         :metadata_prefix => 'oai_dc',
         :from => Time.parse("September 1 2004"),
-        :until => Time.parse("November 30 2004"))
+        :until => Time.parse("December 25 2005"))
       )
     assert_equal (@provider.model.limit + 1), doc.elements["/OAI-PMH/ListRecords"].to_a.size
     assert_not_nil doc.elements["/OAI-PMH/ListRecords/resumptionToken"]
     token = doc.elements["/OAI-PMH/ListRecords/resumptionToken"].text
 
-    doc = Document.new(@provider.list_records(:resumption_token => token))
-    assert_not_nil doc.elements["/OAI-PMH/ListRecords/resumptionToken"]
-    assert_equal (@provider.model.limit + 1), doc.elements["/OAI-PMH/ListRecords"].to_a.size
-    token = doc.elements["/OAI-PMH/ListRecords/resumptionToken"].text
+    4.times do
+      doc = Document.new(@provider.list_records(:resumption_token => token))
+      assert_not_nil doc.elements["/OAI-PMH/ListRecords/resumptionToken"]
+      assert_equal (@provider.model.limit + 1), doc.elements["/OAI-PMH/ListRecords"].to_a.size
+      token = doc.elements["/OAI-PMH/ListRecords/resumptionToken"].text
+    end
 
     doc = Document.new(@provider.list_records(:resumption_token => token))
     # assert that ListRecords includes remaining records and an empty resumption token
-    assert_equal (301 % @provider.model.limit), doc.elements["/OAI-PMH/ListRecords"].to_a.size
+    assert_equal (551 % @provider.model.limit), doc.elements["/OAI-PMH/ListRecords"].to_a.size
     assert_not_nil doc.elements["/OAI-PMH/ListRecords/resumptionToken"]
     assert_nil doc.elements["/OAI-PMH/ListRecords/resumptionToken"].text
   end

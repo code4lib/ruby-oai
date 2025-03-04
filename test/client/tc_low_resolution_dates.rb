3ed30ac378036dab9f39575c4738bac3d65cf6ec
Jonathan Rochkind <jonathan@dnil.net>
2025-03-03 19:57:43 -0500

    test against local OAI server, not rudely against some other unreliable one on internet

--- test/client/tc_low_resolution_dates.rb
+++ test/client/tc_low_resolution_dates.rb
@@ -2,11 +2,8 @@ require 'test_helper_client'
 
 class LowResolutionDatesTest < Test::Unit::TestCase
 
-  # We really should not be testing against a live OAI server, come on!
-  # It could go away!  But I'm not sure how to make this test reasonable,
-  # what it's really testing.
   def test_low_res_date_parsing
-    client = OAI::Client.new 'http://authors.library.caltech.edu/oai2d'
+    client = OAI::Client.new 'http://localhost:3333/oai'
 
     date = Date.new 2003, 1, 1
 

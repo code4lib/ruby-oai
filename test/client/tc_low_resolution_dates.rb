b2f6c41256c6129c6dc81dc05002b5f1ff7702cd
Jonathan Rochkind <jonathan@dnil.net>
2025-03-03 19:23:41 -0500

    fix to current caltech OAI uri for test that is really rudely using a live OAI server, come on, sigh

--- test/client/tc_low_resolution_dates.rb
+++ test/client/tc_low_resolution_dates.rb
@@ -2,8 +2,11 @@ require 'test_helper_client'
 
 class LowResolutionDatesTest < Test::Unit::TestCase
 
+  # We really should not be testing against a live OAI server, come on!
+  # It could go away!  But I'm not sure how to make this test reasonable,
+  # what it's really testing.
   def test_low_res_date_parsing
-    client = OAI::Client.new 'http://authors.library.caltech.edu/cgi/oai2'
+    client = OAI::Client.new 'http://authors.library.caltech.edu/oai2d'
 
     date = Date.new 2003, 1, 1
 
